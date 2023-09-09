c47e1f8a1fe00549fe34366f28e85bf7340c60c7
Alexander Skvorchevskiy <sashaskvorhevskiy@gmail.com>
2023-09-09 13:43:38 +0300

    Initial project

---
+++ app/auth/auth.controller.js
@@ -0,0 +1,14 @@
+import { prisma } from '../prisma.js'
+
+/**
+ * Auth user.
+ * @route POST /api/auth/login
+ * @access Public
+ */
+export const authUser = async (req, res) => {
+	const user = await prisma.user.findMany()
+
+	res.json({
+		user
+	})
+}
---
+++ server.js
@@ -0,0 +1,36 @@
+import dotenv from 'dotenv'
+import express from 'express'
+
+import morgan from 'morgan'
+import authRouter from './app/auth/auth.routes.js'
+import { prisma } from './app/prisma.js'
+
+dotenv.config()
+
+const app = express()
+
+async function main() {
+	if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
+
+	app.use(express.json())
+	app.use('/api/auth', authRouter)
+
+	const PORT = process.env.PORT || 5000
+
+	app.listen(
+		PORT,
+		console.log(
+			`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+		)
+	)
+}
+
+main()
+	.then(async () => {
+		await prisma.$disconnect()
+	})
+	.catch(async e => {
+		console.error(e)
+		await prisma.$disconnect()
+		process.exit(1)
+	})

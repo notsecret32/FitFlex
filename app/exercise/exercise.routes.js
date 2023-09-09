8cf2aa7c1baf207d9398321eada952cfff9bca95
Alexander Skvorchevskiy <sashaskvorhevskiy@gmail.com>
2023-09-09 23:48:21 +0300

    feat: added new functionality and endpoints for their operation

--- app/exercise/exercise.routes.js
+++ app/exercise/exercise.routes.js
@@ -8,6 +8,12 @@ import {
 	getExercises,
 	updateExercise
 } from './exercise.controller.js'
+import { createNewExerciseLog } from './log/exercise-log.controller.js'
+import { getExerciseLog } from './log/get-exercise-log.controller.js'
+import {
+	completeExerciseLog,
+	updateExerciseLogTime
+} from './log/update-exercise-log.controller.js'
 
 const router = express.Router()
 
@@ -20,5 +26,17 @@ router
 	.get(protect, getExercise)
 	.put(protect, updateExercise)
 	.delete(protect, deleteExercise)
+
+// Route: /api/exercises/log/:id
+router
+	.route('/log/:id')
+	.get(protect, getExerciseLog)
+	.post(protect, createNewExerciseLog)
+
+// Route: /api/exercises/log/time/:id
+router.route('/log/time/:id').put(protect, updateExerciseLogTime)
+
+// Route: /api/exercises/log/complete/:id
+router.route('/log/complete/:id').patch(protect, completeExerciseLog)
 
 export default router

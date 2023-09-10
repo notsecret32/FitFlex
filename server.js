f8bc27382d494e0dfc388d918efd6a362ddba5ed
Alexander Skvorchevskiy <sashaskvorhevskiy@gmail.com>
2023-09-10 19:41:24 +0300

    feat: added cors

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
 import morgan from 'morgan'
@@ -17,6 +18,7 @@ const app = express()
 async function main() {
 	if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
 
+	app.use(cors())
 	app.use(express.json())
 
 	const __dirname = path.resolve()

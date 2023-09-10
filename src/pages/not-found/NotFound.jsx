4bacef55b62cdab853bfffb900d1d8bfaa3ae054
Alexander Skvorchevskiy <sashaskvorhevskiy@gmail.com>
2023-09-11 00:50:01 +0300

    feat: added a new `NotFound` page

---
+++ src/pages/not-found/NotFound.jsx
@@ -0,0 +1,12 @@
+import Layout from '../../components/layout/Layout'
+
+const NotFound = () => {
+	return (
+		<>
+			<Layout heading='Page not found' />
+			<div className='wrapper-inner-page'>404 page not found</div>
+		</>
+	)
+}
+
+export default NotFound

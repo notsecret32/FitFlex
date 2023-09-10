40b7af344f9c205c4b3abae84317a02eceba4e03
Alexander Skvorchevskiy <sashaskvorhevskiy@gmail.com>
2023-09-11 01:06:27 +0300

    fix: fixed a bug where the `Header` was drawn twice

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,23 +1,21 @@
-import {
-	Route,
-	createBrowserRouter,
-	createRoutesFromElements
-} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import NotFound from '../pages/not-found/NotFound'
 import { routes } from './routes.data'
 
-export const router = createBrowserRouter(
-	createRoutesFromElements(
-		<Route path='/'>
-			{routes.map(route => (
-				<Route
-					key={route.path}
-					path={route.path}
-					element={<route.component />}
-				/>
-			))}
-
-			<Route path='*' element={<NotFound />} />
-		</Route>
+export const Router = () => {
+	return (
+		<BrowserRouter>
+			<Routes>
+				{/* TODO: Auth routes */}
+				{routes.map(route => (
+					<Route
+						key={route.path}
+						path={route.path}
+						element={<route.component />}
+					/>
+				))}
+				<Route path='*' element={<NotFound />} />
+			</Routes>
+		</BrowserRouter>
 	)
-)
+}

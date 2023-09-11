f1f296f6110bb8a258bdd36e63d75bc0ec096185
Alexander Skvorchevskiy <sashaskvorhevskiy@gmail.com>
2023-09-11 14:33:49 +0300

    fix: fixed a bug where the application always went to a certain path

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -2,12 +2,15 @@ import { FiArrowLeft } from 'react-icons/fi'
 import { SlUser } from 'react-icons/sl'
 import { useLocation, useNavigate } from 'react-router-dom'
 
+import { useAuth } from '../../../hooks/useAuth'
 import Hamburger from '../hamburger/Hamburger'
 import styles from './Header.module.scss'
 
 const Header = ({ backLink = '' }) => {
 	const { pathname } = useLocation()
 	const navigate = useNavigate()
+
+	const { isAuth } = useAuth()
 
 	return (
 		<header className={styles.header}>
@@ -16,7 +19,7 @@ const Header = ({ backLink = '' }) => {
 					<FiArrowLeft color='white' fontSize={30} />
 				</button>
 			) : (
-				<button onClick={() => navigate('/profile')}>
+				<button onClick={() => navigate(isAuth ? '/profile' : '/auth')}>
 					<SlUser fill='#fff' fontSize={30} />
 				</button>
 			)}

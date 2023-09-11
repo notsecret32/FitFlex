72167c5662cfc32a02905e5c8efbf817ec02ed10
Alexander Skvorchevskiy <sashaskvorhevskiy@gmail.com>
2023-09-11 20:31:53 +0300

    refactor: the endpoints have been moved to a separate file

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,12 +1,13 @@
 import Cookies from 'js-cookie'
 
 import { $axios } from '../api'
+import { API_AUTH } from '../constants/api.constants'
 import { COOKIE_USER_TOKEN } from '../constants/cookie.constants'
 
 export const AuthService = {
 	main: async (email, password, type) => {
 		try {
-			const { data } = await $axios.post(`/auth/${type}`, {
+			const { data } = await $axios.post(`${API_AUTH}/${type}`, {
 				email,
 				password
 			})

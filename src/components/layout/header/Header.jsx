685c979f1e956462a6c6bef5fac186fac01f9228
Alexander Skvorchevskiy <sashaskvorhevskiy@gmail.com>
2023-09-11 13:05:45 +0300

    feat: added new functionality for `Header` component

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,14 +1,25 @@
 import { FiArrowLeft } from 'react-icons/fi'
+import { SlUser } from 'react-icons/sl'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 import Hamburger from '../hamburger/Hamburger'
 import styles from './Header.module.scss'
 
-const Header = () => {
+const Header = ({ backLink = '' }) => {
+	const { pathname } = useLocation()
+	const navigate = useNavigate()
+
 	return (
 		<header className={styles.header}>
-			<button onClick={() => {}}>
-				<FiArrowLeft color='white' fontSize={30} />
-			</button>
+			{pathname !== '/' ? (
+				<button onClick={() => navigate(backLink || '/')}>
+					<FiArrowLeft color='white' fontSize={30} />
+				</button>
+			) : (
+				<button onClick={() => navigate('/profile')}>
+					<SlUser fill='#fff' fontSize={30} />
+				</button>
+			)}
 			<Hamburger />
 		</header>
 	)

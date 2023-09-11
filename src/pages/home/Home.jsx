33858b189499ff8ed135a9b4ad50e10534942d73
Alexander Skvorchevskiy <sashaskvorhevskiy@gmail.com>
2023-09-11 13:06:48 +0300

    feat: added new functionality for `Home` component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,22 @@
+import { useNavigate } from 'react-router-dom'
+
+import Layout from '../../components/layout/Layout'
+import Button from '../../components/ui/button/Button'
+import { useAuth } from '../../hooks/useAuth'
+import styles from './Home.module.scss'
+
 const Home = () => {
-	return <div>Home component</div>
+	const { isAuth } = useAuth()
+	const navigate = useNavigate()
+
+	return (
+		<Layout bgImage='/images/home-bg.png'>
+			<Button clickHandler={() => navigate(isAuth ? '/new-workout' : '/auth')}>
+				{isAuth ? 'New' : 'Sign In'}
+			</Button>
+			<h1 className={styles.heading}>EXERCISE FOR THE SHOULDERS</h1>
+		</Layout>
+	)
 }
 
 export default Home

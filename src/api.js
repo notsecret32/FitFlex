241a3b9c90d1f4a4618224053cdefb3d5510b962
Alexander Skvorchevskiy <sashaskvorhevskiy@gmail.com>
2023-09-12 00:03:44 +0300

    refactor: added `.env` file support

--- src/api.js
+++ src/api.js
@@ -2,10 +2,8 @@ import axios from 'axios'
 import Cookies from 'js-cookie'
 import { COOKIE_USER_TOKEN } from './constants/cookie.constants'
 
-const API_URL = 'http://localhost:5000/api'
-
 export const $axios = axios.create({
-	baseURL: API_URL,
+	baseURL: `${import.meta.env.VITE_SERVER_URL}/api`,
 	headers: {
 		'Content-Type': 'application/json',
 		Authorization: `Bearer ${Cookies.get(COOKIE_USER_TOKEN)}`

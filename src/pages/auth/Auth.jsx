d12202f857254a74a5cf8630fb464aaa7fe812a0
Alexander Skvorchevskiy <sashaskvorhevskiy@gmail.com>
2023-09-11 16:15:55 +0300

    feat: authorization support has been added

--- src/pages/auth/Auth.jsx
+++ src/pages/auth/Auth.jsx
@@ -1,30 +1,48 @@
+import { useMutation } from '@tanstack/react-query'
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 
-import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import Layout from '../../components/layout/Layout'
 import Button from '../../components/ui/button/Button'
 import Field from '../../components/ui/field/Field'
+import Loader from '../../components/ui/loader/Loader'
+import { AuthService } from '../../services/auth.service'
 import styles from './Auth.module.scss'
 
 const Auth = () => {
-	const [type, setType] = useState('auth')
+	const [type, setType] = useState('login')
+	const navigate = useNavigate()
 
 	const {
 		register,
 		handleSubmit,
-		formState: { errors }
+		formState: { errors },
+		reset
 	} = useForm({
 		mode: 'onChange'
 	})
 
+	const { mutate, isLoading } = useMutation(
+		['auth'],
+		({ email, password }) => AuthService.main(email, password, type),
+		{
+			onSuccess: () => {
+				reset()
+				navigate('/')
+			}
+		}
+	)
+
 	const onSubmit = data => {
-		console.log(data)
+		mutate(data)
 	}
 
 	return (
 		<>
 			<Layout heading='Sign In' bgImage='/images/auth-bg.png' />
 			<div className='wrapper-inner-page'>
+				{isLoading && <Loader />}
 				<form onSubmit={handleSubmit(onSubmit)}>
 					<Field
 						error={errors?.email?.message}
@@ -47,7 +65,7 @@ const Auth = () => {
 						placeholder='Enter password'
 					/>
 					<div className={styles.wrapperButtons}>
-						<Button clickHandler={() => setType('auth')}>Sign in</Button>
+						<Button clickHandler={() => setType('login')}>Sign in</Button>
 						<Button clickHandler={() => setType('register')}>Sign up</Button>
 					</div>
 				</form>

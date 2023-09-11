a8f95988b52bbdbef529a924429ebe629614458e
Alexander Skvorchevskiy <sashaskvorhevskiy@gmail.com>
2023-09-11 14:34:20 +0300

    feat: the authorization page has been updated

--- src/pages/auth/Auth.jsx
+++ src/pages/auth/Auth.jsx
@@ -1,5 +1,59 @@
+import { useForm } from 'react-hook-form'
+
+import { useState } from 'react'
+import Layout from '../../components/layout/Layout'
+import Button from '../../components/ui/button/Button'
+import Field from '../../components/ui/field/Field'
+import styles from './Auth.module.scss'
+
 const Auth = () => {
-	return <div>Auth page</div>
+	const [type, setType] = useState('auth')
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors }
+	} = useForm({
+		mode: 'onChange'
+	})
+
+	const onSubmit = data => {
+		console.log(data)
+	}
+
+	return (
+		<>
+			<Layout heading='Sign In' bgImage='/images/auth-bg.png' />
+			<div className='wrapper-inner-page'>
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<Field
+						error={errors?.email?.message}
+						name='email'
+						register={register}
+						options={{
+							required: 'Email is required'
+						}}
+						type='email'
+						placeholder='Enter email'
+					/>
+					<Field
+						error={errors?.password?.message}
+						name='password'
+						register={register}
+						options={{
+							required: 'Password is required'
+						}}
+						type='password'
+						placeholder='Enter password'
+					/>
+					<div className={styles.wrapperButtons}>
+						<Button clickHandler={() => setType('auth')}>Sign in</Button>
+						<Button clickHandler={() => setType('register')}>Sign up</Button>
+					</div>
+				</form>
+			</div>
+		</>
+	)
 }
 
 export default Auth
